b11d29b5978700da5061dc58004761c17c3a26f4
D14LB3L0 <diegobdev2024@gmail.com>
2025-04-12 09:43:51 -0500

    initialize

---
+++ EcoTrueke.Services/API/APIResponse.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+
+namespace Wayni.Services.API
+{
+    public class APIResponse
+    {
+        public string Body { get; set; }
+
+        public HttpStatusCode Code { get; set; }
+        public APIResponse()
+        {
+            Code = 0;
+            Body = null;
+        }
+        public APIResponse(HttpStatusCode Code, string Body)
+        {
+            this.Code = Code;
+            this.Body = Body;
+        }
+    }
+}
+
---
+++ EcoTrueke.Services/API/AuthorizationHeader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace Wayni.Services.API
+{
+    public class AuthorizationHeader
+    {
+        public string Type { get; set; }
+        public string AuthenticationString { get; set; }
+
+        public AuthorizationHeader(string token)
+        {
+            Type = TYPE_BEARER_TOKEN;
+            AuthenticationString = token;
+        }
+        public AuthorizationHeader(string username, string password)
+        {
+            Type = TYPE_BASIC;
+            AuthenticationString = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{username}:{password}"));
+        }
+
+        public const string TYPE_BEARER_TOKEN = "Bearer";
+        public const string TYPE_BASIC = "Basic";
+    }
+}
