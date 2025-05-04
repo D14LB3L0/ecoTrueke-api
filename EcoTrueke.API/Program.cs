4db6c9474a7c81b43a9238d7f5229b616c8bead1
D14LB3L0 <diegobdev2024@gmail.com>
2025-05-04 17:09:04 +0100

    remove swagger

--- EcoTrueke.API/Program.cs
+++ EcoTrueke.API/Program.cs
@@ -33,11 +33,11 @@ builder.Services.AddHttpContextAccessor();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
