95ef9e85e4d2fe469ef6c8491e93193022991308
D14LB3L0 <diegobdev2024@gmail.com>
2025-06-28 23:24:51 +0100

    fix notification link

--- EcoTrueke.Domain/Constants/Notifications.cs
+++ EcoTrueke.Domain/Constants/Notifications.cs
@@ -14,7 +14,7 @@
             public const string Title = "Solicitud de intercambio";
             public const string Message = "Haz recibido una solictud de intecambio";
             public const string Type = "info";
-            public const string Link = "/dashboard/my-products/proposal";
+            public const string Link = "/dashboard/my-products/requested";
         }
         public static class OwnerUserAcceptedProposal
         {
